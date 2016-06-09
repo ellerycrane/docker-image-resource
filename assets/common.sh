2836b7601bcfb73cd2123f8ce186e2bea01893f4
Ellery Crane <ellery@pwnieexpress.com>
2016-06-09 15:37:36 -0400

    Added exporting of docker image with build cache to remote server on build.

--- assets/common.sh
+++ assets/common.sh
@@ -144,3 +144,27 @@ docker_pull() {
   printf "\n${RED}Failed to pull image %s.${NC}" "$1"
   exit 1
 }
+
+export TMPDIR=${TMPDIR:-/tmp}
+
+load_pubkey() {
+  local private_key_path=$TMPDIR/build-cache-private-key
+
+  (jq -r '.source.build_cache.private_key // empty' < $1) > $private_key_path
+
+  if [ -s $private_key_path ]; then
+    chmod 0600 $private_key_path
+
+    eval $(ssh-agent) >/dev/null 2>&1
+    trap "kill $SSH_AGENT_PID" 0
+
+    SSH_ASKPASS=/opt/resource/askpass.sh DISPLAY= ssh-add $private_key_path >/dev/null
+
+    mkdir -p ~/.ssh
+    cat > ~/.ssh/config <<EOF
+StrictHostKeyChecking no
+LogLevel quiet
+EOF
+    chmod 0600 ~/.ssh/config
+  fi
+}
